c28feef7cb206069efc1baccb0a6cfdfd32ed341
Habiba553 <ummekabir684@gmail.com>
2026-05-23 20:12:08 +0600

    initial client setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl text-center mt-20 font-bold">
+        Public Infrastructure Issue Reporting System
+      </h1>
+    </div>
+  )
+}
+
+export default App
